84366ef93ff077b2e6c1ea08aa78dfb904b6b14c
J. Heasly <jheasly@earthlink.net>
2011-09-12 17:53:05 -0700

    Preview for obituary!

--- obituary/utils.py
+++ obituary/utils.py
@@ -10,3 +10,16 @@ def delete_old_items():
     days_back = datetime.timedelta(days=DATABASE_DAYS_BACK)
     old_items = Death_notice.objects.filter(death_notice_created__lte= (datetime.datetime.now() - days_back))
     old_items.delete()
+
+def output_cleanup_hack(data):
+    data = data.replace('..', '.')
+    data = data.replace('  ', ' ')
+    data = data.replace('; ; ', '; ')
+    data = data.replace('> ', '>')
+    return data
+
+def adobe_to_web(input_string):
+    output_list = [ u'<p>%s</p>' % line for line in input_string.split('\n') ]
+    output_list[0] = u'<b>%s</b>' % output_list[0]
+    output_string = u'\n'.join(output_list)
+    return output_string
--- obituary/views.py
+++ obituary/views.py
@@ -18,6 +18,7 @@ from obituary.forms import Death_noticeForm, \
     ServiceFormSet, ObituaryForm, VisitationFormSet, BEI_FormSet, \
     Other_servicesFormSet, ChildrenFormSet, SiblingsFormSet, MarriageFormSet, \
     DeathNoticeOtherServicesFormSet
+from obituary.utils import output_cleanup_hack, adobe_to_web
 
 from obituary_settings import DISPLAY_DAYS_BACK
 
@@ -130,9 +131,15 @@ def manage_death_notice(request, death_notice_id=None):
         'death_notice_id': death_notice_id,
     }
     
+    # To be used for Preview
     if death_notice_id:
         response_dict['object_list'] = [death_notice]
     
+    t = loader.get_template('manage_death_notice.html')
+    c = RequestContext(request, response_dict)
+    data = t.render(c)
+    r = HttpResponse(data)
+    return r
     return render_to_response('manage_death_notice.html', response_dict, context_instance=RequestContext(request))
 
 # http://docs.djangoproject.com/en/1.3/topics/forms/modelforms/
@@ -192,6 +199,7 @@ def manage_obituary(request, obituary_id=None):
                 return HttpResponseRedirect(reverse('obituary.views.manage_obituary', args=(obituary.pk,)))
     else:
         if obituary_id:
+            current_obit = Obituary.objects.get(pk=obituary_id)
             form = ObituaryForm(request, instance=obituary)
             formset = VisitationFormSet(instance=obituary)
             bei_formset = BEI_FormSet(instance=obituary)
@@ -208,7 +216,7 @@ def manage_obituary(request, obituary_id=None):
             sib_formset = SiblingsFormSet(instance=Obituary())
             wed_formset = MarriageFormSet(instance=Obituary())
     
-    return render_to_response('manage_obituary.html', {
+    ob_response_dict = {
         'form': form,
         'formset_name': formset.model._meta.verbose_name,
         'formset': formset,
@@ -221,7 +229,21 @@ def manage_obituary(request, obituary_id=None):
         'sib_formset_name': sib_formset.model._meta.verbose_name,
         'sib_formset': sib_formset,
         'wed_formset': wed_formset,
-    }, context_instance=RequestContext(request))
+        'obituary_id': obituary_id,
+    }
+    
+    # To be used for Preview
+    if obituary_id:
+        tm = loader.get_template('obituary_list_unix_include.txt')
+        ct = RequestContext(request, {'object_list': [current_obit]})
+#         ct = RequestContext(request)
+#         ct['object_list'] = [current_obit]
+        dt = tm.render(ct)
+        dt = output_cleanup_hack(dt)
+        dt = adobe_to_web(dt)
+        ob_response_dict['obit_preview'] = dt
+    
+    return render_to_response('manage_obituary.html', ob_response_dict, context_instance=RequestContext(request))
 
 def logout_view(request):
     logout(request)
