f8f203ac9992baa5eee7c1492ef60a91d120cc1a
oper <oper@ip-10-162-166-191.us-west-1.compute.internal>
2011-09-13 14:17:08 -0700

    Expanded BEI.bei CharField to 20 chars.

--- obituary/models.py
+++ obituary/models.py
@@ -692,7 +692,7 @@ class BEI(models.Model):
     )
     
     obituary = models.OneToOneField(Obituary)
-    bei = models.CharField(u'burial, entombment or inurnment', choices=BEI, max_length=10)
+    bei = models.CharField(u'burial, entombment or inurnment', choices=BEI, max_length=20)
     bei_date_time = models.CharField(u'burial, entombment or inurnment date and time', max_length=110, blank=True, null=True)
     bei_location = models.CharField(u'burial, entombment or inurnment location', max_length=126)
     
