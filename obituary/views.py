7f2b400699347775f02b9d3cdc99ec07fdc539a2
J. Heasly <jheasly@earthlink.net>
2011-09-15 13:35:52 -0700

    Fixed views error.

--- obituary/views.py
+++ obituary/views.py
@@ -43,8 +43,8 @@ def deaths(request, model=None, file=None):
     
     t = loader.get_template(template_name)
     c = RequestContext(request, {'object_list': queryset })
-    data = data.replace('..', '.')
     data = t.render(c).replace('  ', ' ')
+    data = data.replace('..', '.')
     data = data.replace('; ; ', '; ')
     data = data.replace('> ', '>')
     r = HttpResponse(data, mimetype='text/plain;charset=utf-8')
