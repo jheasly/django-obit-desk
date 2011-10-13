1cbfb0d7e4fb05ff2d8e3dd1c9bb0798fb795047
J. Heasly <jheasly@earthlink.net>
2011-10-13 13:57:46 -0700

    Added admin list_filters for payment tracking.

--- obituary/admin.py
+++ obituary/admin.py
@@ -50,6 +50,7 @@ class ObituaryAdmin(AdminImageMixin, admin.ModelAdmin):
     list_display = ('death_notice', 'ready_for_print', 'gender', 'date_of_birth', 'service_date', 'obituary_created', 'admin_thumbnail', 'display_photo_file_name', 'obituary_in_system', 'obituary_has_run', 'status',)
     list_editable = ('obituary_in_system', 'obituary_has_run')
     search_fields = ['death_notice__last_name', 'death_notice__first_name',]
+    list_filter = ('death_notice__death_date', 'obituary_has_run',)
     
     inlines = [
         MarriageInline,
